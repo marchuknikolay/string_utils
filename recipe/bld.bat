85fbbdd8e0c67b0295f107c02e88f8089b680ab8
Mykola Marchuk <mmarchuk@Mykolas-MacBook-Pro.local>
2025-03-30 11:20:45 +0300

    add copying of necessary files

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,13 @@
 @echo off
 
+rmdir /s /q build
 mkdir build
 cd build
 
-cmake .. -G Ninja
-ninja
+cmake .. ^
+    -G Ninja ^
+    -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+    -DCMAKE_PREFIX_PATH=%PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release
+
+ninja install
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-mkdir build
+rm -rf build
+mkdir -p build
 cd build
 
-cmake .. -G Ninja
-ninja
+cmake .. \
+    -G Ninja \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release
+
+ninja install
